d8bc3e89a9a26dcb3fea4b4cab82a0100be8c7f6
Andreas Paul <github@elcaptian.fastmail.com>
2021-12-13 22:13:30 +0400

    Plotting image

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-
+#%%
+import rasterio
+import matplotlib.pyplot as plt
 from pathlib import Path
 from utils import get_dem_data, load_shapefile, crop_raster, save_raster
 
 
 dem_path: Path = Path("https://planetarymaps.usgs.gov/mosaic/Mars_MGS_MOLA_DEM_mosaic_global_463m.tif")
 cropped_raster_path: Path = Path("data/RGB-byte-masked-mola-dem.tif")
-raster_path: Path = Path("data/mola_dem.tif")
+output_raster_path: Path = Path("data/mola_dem.tif")
 shp_path: Path = Path("data/extent.shp")
 
 
+get_dem_data(dem_path, output_raster_path)
+shapes = load_shapefile(shp_path)
+img, transform, meta = crop_raster(output_raster_path, shapes)
+save_raster(cropped_raster_path, meta, img)
 
-if __name__ == "__main__":
-    get_dem_data(dem_path, raster_path)
-    shapes = load_shapefile(shp_path)
-    img, transform, meta = crop_raster(raster_path, shapes)
-    save_raster(cropped_raster_path, meta, img)
+src = rasterio.open("data/RGB-byte-masked-mola-dem.tif")
+plt.imshow(src.read(1), cmap="Reds_r")
+plt.show()
+
+src = rasterio.open("data/RGB-byte-masked-mola-dem.tif")
+plt.imshow(src.read(1), cmap="Greys")
+plt.show()
 
 
 
--- utils.py
+++ utils.py
@@ -10,11 +10,7 @@ from pathlib import Path
 
 def get_dem_data(path: Path, local_file_name: Path):
     if not os.path.exists(local_file_name):
-        print("Downloading file")
         urllib.request.urlretrieve(path, local_file_name)
-        print("Finished downloading file")
-    else:
-        print("File exists.")
 
 
 def load_shapefile(path_to_shp: Path):
@@ -35,10 +31,22 @@ def crop_raster(path_to_raster: Path, shapes: list):
 
 
 def save_raster(path_to_raster: Path, meta, img):
-    with rasterio.open(path_to_raster, "w", **meta) as dest:
-        dest.write(img)
+    if not os.path.exists(path_to_raster):
+        with rasterio.open(path_to_raster, "w", **meta) as dest:
+            dest.write(img)
         
     
 def rasterize_vector(path_to_vector: Path, out_file_name: Path, feature_name: str, resolution: tuple = (-0.0001, 0.0001)):    
     out_grid = make_geocube(vector_data=path_to_vector, resolution=resolution)
     out_grid[feature_name].rio.to_raster(out_file_name)
+
+
+def custom_color_map():
+    """
+    Example usage of returns: show(src,cmap=cmap,norm=norm,interpolation='bilinear')
+    """
+    levels = [0, 0.01, 0.02, 0.04, 0.8, 1, 2, 4, 7, 17, 70, 500]
+    clrs = ['#E3E8E81A','#3DFDFF8C', '#3CFAFF', '#6A95FF', '#E61AFE8C', '#FF04FF', '#f4ded9', '#f4dbaa', '#eda14f', '#e87511', '#b55400', '#633b11']    
+    cmap, norm = colors.from_levels_and_colors(levels, clrs, extend='max')
+    
+    return cmap, norm
