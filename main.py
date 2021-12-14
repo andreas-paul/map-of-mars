71205dafdafccdeac76f05aff1ede5fb1911a5a2
Andreas Paul <github@elcaptian.fastmail.com>
2021-12-14 23:25:24 +0400

    Code improvements & fixes

--- main.py
+++ main.py
@@ -18,13 +18,18 @@ shapes = load_shapefile(shp_path)
 img, transform, meta = crop_raster(output_raster_path, shapes)
 save_raster(cropped_raster_path, meta, img)
 
+cm_reds = make_ramp(['#bd4628', '#c15033', '#c45a3f', '#c8644b', '#cc6e56', '#cf7962', 
+                    '#d3836e', '#d78d7a', '#db9785', '#dea291', '#e2ac9d', '#e6b6a8', 
+                    '#e9c0b4', '#edcac0', '#f1d5cc', '#f4dfd7', '#f8e9e3', '#fcf3ef', '#fffdfa'])
+
+cm_grays = make_ramp(['#000000', '#000000', '#FFFFFF'] )
+
 src = rasterio.open("data/RGB-byte-masked-mola-dem.tif")
-plt.imshow(src.read(1), cmap="Reds_r")
-plt.show()
-
-colormap = make_ramp(['#000000', '#000000', '#FFFFFF'] )
+plt.imshow(src.read(1), cmap=cm_reds)
+plt.axis('off')
+plt.Axes(fig, [0,0,1,1]) # Remove whitespace
 
 src = rasterio.open("data/RGB-byte-masked-mola-dem.tif")
-plt.imshow(src.read(1), cmap=colormap)
-plt.show()
-
+plt.imshow(src.read(1), cmap=cm_grays)
+plt.axis('off')
+plt.Axes(fig, [0,0,1,1])
